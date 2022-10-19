9cc5183d48f83456f677651801e458109590c5e9
= <=>
2022-10-19 18:35:32 +0200

    impl UI

--- app/src/main/java/com/salma/ktorclient/MainActivity.kt
+++ app/src/main/java/com/salma/ktorclient/MainActivity.kt
@@ -3,38 +3,52 @@ package com.salma.ktorclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.salma.ktorclient.remote.PostsService
+import com.salma.ktorclient.remote.data.dto.PostResponse
 import com.salma.ktorclient.ui.theme.KtorClientTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val service = PostsService.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KtorClientTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    val posts = produceState<List<PostResponse>>(
+                        initialValue = emptyList(),
+                        producer = {
+                            value = service.getPosts()
+                        }
+                    )
+                    LazyColumn{
+                        items(posts.value) {
+                            Column(modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)) {
+                                Text(text = it.title, fontSize = 20.sp)
+                                Spacer(modifier = Modifier.height(4.dp))
+                                Text(text = it.body, fontSize = 14.sp)
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KtorClientTheme {
-        Greeting("Android")
     }
 }
